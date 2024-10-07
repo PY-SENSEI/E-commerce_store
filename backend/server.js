5c8e12948619c661679a653123041734917d8c71
PY-SENSEI <gauravjena48@gmail.com>
2024-10-07 20:15:13 +0530

    1-backend-completed

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // neccessary to parse the request body data
+app.use(cookieParser()); // for parsing or accessing the cookies 
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server running on port: ${PORT}`);
+
+    connectDB();
+});
